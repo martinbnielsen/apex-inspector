d7fc6f5f805535e972645816d01e563c9631deb9
Martin Nielsen <mnielsen@nokomishealth.com>
2023-01-10 21:34:09 +0100

    misc improvements

--- src/js/inspector.js
+++ src/js/inspector.js
@@ -14,10 +14,12 @@ var mbninspector = {};
       const _selAPEXToolbar     = '#apexDevToolbar';
       const _selInspectorIcon   = '#mbnInspector';
       const _selHover           = 'body form *';
+      const _selOverlay         = '#mbnInspectorOverlay';
       const _htmlInspectorIcon  = '<li role="none">' +
                                   '<button type="button" class="a-Button a-Button--devToolbar" id="mbnInspector" title="APEX Inspector" aria-label="APEX Inspector">' +
                                   '<span class="fa fa-hand-pointer-o" aria-hidden="true"></span>' +
                                   '</button></li>';
+      const _htmlOverlay        = '<div id="mbnInspectorOverlay" style="display:none"/>';
       const _selSelectors       = [
                                     {class: 't-Button',       type: 'BUTTON'},
                                     {class: 'text_field',     type: 'ITEM'},
@@ -28,17 +30,182 @@ var mbninspector = {};
 
       let _isActive = false;
       let _config = {
-        delay: 800
+        delay: 800,
+        output: 'screen',
+        overlayHeight: 300,
+        overlayWidth: 700,
+        overlayScroll: 4,
+        shortcutKey: 'I',
+        messageTimeout: 1000,
+        showMessage: true
       };
+
       let _lastElement = null;
       let _setTimeoutConst = null;
+      let _inspectorOverlay = null;
+      let _overlayVisible = false;
 
       const myStorage = apex.storage.getScopedLocalStorage({ prefix: "APEX Inspector" });
 
       // Private functions
 
       /**
-       * Show overlay
+       * Stringify JS Object as HTML
+       * Based on:
+       * https://github.com/JedWatson/html-stringify
+       */
+      function _htmlStringify(obj, fromRecur) {
+
+        var tag = (fromRecur) ? 'span' : 'div';
+        var nextLevel = (fromRecur || 0) + 1;
+        
+        // strings
+        if (typeof obj == 'string') {
+          return '<' + tag + ' style="color: #0e4889; cursor: default;">"' + obj + '"</' + tag + '>';
+        }
+        // booleans, null and undefined
+        else if (typeof obj == 'boolean' || obj === null || obj === undefined) {
+          return '<' + tag + '><em style="color: #06624b; cursor: default;">' + obj + '</em></' + tag + '>';
+        }
+        // numbers
+        else if (typeof obj == 'number') {
+          return '<' + tag + ' style="color: #ca000a; cursor: default;">' + obj + '</' + tag + '>';
+        }
+        // dates
+        else if (Object.prototype.toString.call(obj) == '[object Date]') {
+          return '<' + tag + ' style="color: #009f7b; cursor: default;">' + obj + '</' + tag + '>';
+        }
+        // arrays
+        else if (Array.isArray(obj)) {
+          
+          var rtn = '<' + tag + ' style="color: #666; cursor: default;">: [';
+          
+          if (!obj.length) {
+            return rtn + ']</' + tag + '>';
+          }
+          
+          rtn += '</' + tag + '><div style="padding-left: 20px;">';
+          
+          for (var i = 0; i < obj.length; i++) {
+            rtn += '<span></span>' + _htmlStringify(obj[i], nextLevel); // give the DOM structure has as many elements as an object, for collapse behaviour
+            if (i < obj.length - 1) {
+              rtn += ', <br>';
+            }
+          }
+        
+          return rtn + '</div><' + tag + ' style="color: #666">]</' + tag + '>';
+          
+        }
+        // objects
+        else if (obj && typeof obj == 'object') {
+          
+          var rtn = '',
+            len = Object.keys(obj).length;
+        
+          if (fromRecur && !len) {
+            return '<' + tag + ' style="color: #999; cursor: default;">Object: {}</' + tag + '>';
+          }
+          
+          if (fromRecur) {
+            rtn += '<' + tag + ' style="color: #0b89b6">{</' + tag + '><div class="_stringify_recur _stringify_recur_level_' + fromRecur + '" style="padding-left: 20px;">';
+          }
+          
+          for (var key in obj) {
+            if (typeof obj[key] != 'function') {
+              rtn += '<div><span style="padding-right: 5px; cursor: default;">' +key + ':</span>' + _htmlStringify(obj[key], nextLevel) + '</div>';
+            }
+          }
+          
+          if (fromRecur) {
+            rtn += '</div><' + tag + ' style="color: #0b89b6; cursor: default;">}</' + tag + '>';
+          }
+          
+          return rtn;
+          
+        }
+      
+        return '';
+      
+      }
+
+      /**
+       * Handle overlay positioning
+       */
+      function _positionOverlay(event) {
+        let top=0,left=0;
+        const offset = 5
+
+        if (event.pageY + _config.overlayHeight > $(window).height()) {
+          top = event.pageY - _config.overlayHeight - offset;
+        }
+        else {
+          top = event.pageY + offset;
+        }
+
+        if (event.pageX + _config.overlayWidth > $(window).width()) {
+          left = event.pageX - _config.overlayWidth - (5*offset);
+        }
+        else {
+          left = event.pageX + offset;
+        }
+
+        _inspectorOverlay.css({
+          top: top ,
+          left: left
+        });
+      }
+
+      /**
+       * Handle overlay scrolling
+       */
+      function _scrollOverlay(event) {
+        let top = _inspectorOverlay.scrollTop();
+
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+          // scroll up
+          _inspectorOverlay.scrollTop(top-_config.overlayScroll);
+        }
+        else {
+            // scroll down
+            _inspectorOverlay.scrollTop(top+_config.overlayScroll);
+          }
+      }
+
+      /**
+       * Toggle the overlay
+       */
+      function _toggleOverlay(show) {
+        if (show) {
+          _inspectorOverlay.fadeIn();
+          _overlayVisible = true;
+          _inspectorOverlay.scrollTop(0);
+        }
+        else {
+          _inspectorOverlay.fadeOut();
+          _overlayVisible = false;
+        }
+      }
+    
+
+      /**
+       * Dsiplay output
+       */
+      function _displayOutput(data) {
+        if (_config.output == 'console') {
+          console.log(_logPrefix);
+          data.subObject = {hej: 'test', dato: new Date(), flag: false, liste: [1,2,3,4,{name: 'test', value: 'test2'}]};
+          console.table(data);
+        }
+        else {
+          apex.debug.log(_logPrefix, 'Display insepction in window', _inspectorOverlay);
+          data.subObject = {hej: 'test', dato: new Date(), flag: false, liste: [1,2,3,4,{name: 'test', value: 'test2'}]};
+          _inspectorOverlay.html(_htmlStringify(data));
+          _toggleOverlay(true);
+        }
+      }
+
+      /**
+       * Show Inspection output
        */
       function _showInspection(event, el, type) {
         apex.debug.log(_logPrefix + 'Show overlay', event, el, type);
@@ -58,8 +225,7 @@ var mbninspector = {};
               if (type =='ITEM') {
                 data.value = apex.items[el.attr('id')].getValue();
               }
-              console.log(_logPrefix);
-              console.table(data);
+              _displayOutput(data);
             },
             error: function (jqXHR, textStatus, errorThrown) {
               // handle error
@@ -69,6 +235,20 @@ var mbninspector = {};
         // Add client side information to the objec
       }
 
+
+      /**
+       * Show APEX success message and fadeout
+       */
+      function _showSuccess(msg) {
+        if (!_config.showMessage) return;
+
+        apex.message.showPageSuccess(_logPrefix + msg);
+        setTimeout(function() {
+          apex.message.hidePageSuccess();
+        }, _config.messageTimeout);
+      }
+
+
       /**
        * Handle togglings of the inspector (on/off)
        */
@@ -76,12 +256,16 @@ var mbninspector = {};
         apex.debug.log(_logPrefix + 'Toggle active');
 
         if (_isActive) {
+          if (_overlayVisible) {
+            _inspectorOverlay.fadeOut();
+          }
           $(_selInspectorIcon + ' span').removeClass('mbnInspector-active');
           $('.mbnInspector-selected').removeClass('mbnInspector-selected');      
           $(_selHover).off( "mouseenter mouseleave", _hoverHandler);
           console.log(_logPrefix + 'Deactivated');
           _isActive = false;
           myStorage.setItem( 'active', 'no');
+          _showSuccess('Deactivated');
         }
         else {
           $(_selInspectorIcon + ' span').addClass('mbnInspector-active');
@@ -90,6 +274,7 @@ var mbninspector = {};
           _isActive = true;
           _lastElement = null;
           myStorage.setItem( 'active', 'yes');
+          _showSuccess('Activated');
         }
       }
 
@@ -137,6 +322,9 @@ var mbninspector = {};
         }
         else {
           $('.mbnInspector-selected').removeClass('mbnInspector-selected');      
+          if (_overlayVisible) {
+            _toggleOverlay(false);
+          }
           if (_setTimeoutConst) {
             clearTimeout(_setTimeoutConst);
           }
@@ -173,7 +361,27 @@ var mbninspector = {};
             _toggleActive();
           }
 
+          // Initialise the inspector overlay
+          if (_config.output != 'console') {
+            $('body').append(_htmlOverlay);
+            _inspectorOverlay = $(_selOverlay);
+            _inspectorOverlay.height(_config.overlayHeight);
+            _inspectorOverlay.width(_config.overlayWidth);
+            $('body').on('mousemove', _positionOverlay);
+            $(document).on('mousewheel',_scrollOverlay);
+          }
+
           // TODO: Setup shortcut (configurable) to turn on/off
+          if (_config.shortcutKey) {
+            apex.debug.log(_logPrefix + 'Settings shortcut to shift-ctrl-' + _config.shortcutKey);       
+            $(document).on('keydown', function(e) {
+              console.log('keydown', e);
+              if (e.ctrlKey && e.shiftKey && e.key == _config.shortcutKey) {
+                console.log('toggle');
+                _toggleActive();
+              }
+            });
+          }
       };
   
   })();
